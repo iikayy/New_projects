13f6e9b9fff38ad931cf3b338eba4f1bae9ece2c
iikayy <ii_kayy@yahoo.com>
2024-05-19 21:04:28 +0100

    ytrerdfgh

--- blogg/main.py
+++ blogg/main.py
@@ -1,5 +1,6 @@
 from datetime import date
-from flask import Flask, abort, render_template, redirect, url_for, flash, request, current_app, Blueprint, Markup
+from flask import Flask, abort, render_template, redirect, url_for, flash, request, current_app, Blueprint
+from markupsafe import Markup
 from flask_bootstrap import Bootstrap5
 from flask_ckeditor import CKEditor
 # from flask_gravatar import Gravatar
