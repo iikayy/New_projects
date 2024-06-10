85e823758f96ff2cd4c589e211fbef6bc23e4775
iikayy <ii_kayy@yahoo.com>
2024-06-10 12:30:36 +0100

    weyuw

--- shop/server.py
+++ shop/server.py
@@ -139,7 +139,7 @@ async def register_user(
 
 @app.post("/token", response_model=Token)
 async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
-    user = authenticate_user(db, form_data.email, form_data.password)
+    user = authenticate_user(db, form_data.username, form_data.password)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
